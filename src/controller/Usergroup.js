3e31783a810dee83d08350d5539cee83296d1854
SargTeX <martin.christian.bories@gmail.com>
2015-02-26 12:51:35 +0100

    full Usergroup support
    Added new controllers:
    * GET /UsergroupList
    * GET /UsergroupDelete
    * GET /UsergroupEdit
    * POST /UsergroupEdit

--- src/controller/Usergroup.js
+++ src/controller/Usergroup.js
@@ -18,4 +18,55 @@ exports.setup = function(app) {
 			res.send({status: 'success', template: 'UsergroupAdd'});
 		});
 	});
+
+	app.get('/UsergroupList', function(req, res, jump) {
+		Usergroup.find({})
+			.exec(function(err, usergroups) {
+				if (err) return jump(err);
+
+				res.send({template: 'UsergroupList', data: {usergroups: usergroups}});
+			});
+	});
+
+	app.get('/UsergroupDelete', function(req, res, jump) {
+		Usergroup.findById(req.query.usergroupId)
+			.exec(function(err, usergroup) {
+				if (err) return jump(err);
+				if (!usergroup) return res.send({status: 'success', template: 'UsergroupDelete'});
+
+				usergroup.remove(function(err) {
+					if (err) return jump(err);
+
+					res.send({status: 'success', template: 'UsergroupDelete', data: {name: usergroup.name}});
+				});
+			});
+	});
+
+	app.get('/UsergroupEdit', function(req, res, jump) {
+		Usergroup.findById(req.query.usergroupId)
+			.exec(function(err, usergroup) {
+				if (err) return jump(err);
+				if (!usergroup) return res.send({status: 'error', template: 'Error', errors: ['Die Benutzergruppe existiert nicht.']});
+
+				res.send({template: 'UsergroupEdit', data: {usergroup: usergroup}});
+			});
+	});
+
+	app.post('/UsergroupEdit', function(req, res, jump) {
+		Usergroup.findById(req.body.usergroupId)
+			.exec(function(err, usergroup) {
+				if (err) return jump(err);
+				if (!usergroup) return res.send({status: 'error', template: 'Error', errors: ['Die Benutzergruppe existiert nicht.']});
+
+				usergroup.name = req.body.name;
+				usergroup.save(function(err) {
+					if (err) {
+						if (err.code == 11000) return res.send({status: 'error', template: 'UsergroupEdit', data: {usergroup: usergroup}, errors: ['Der Gruppenname wird bereits verwendet.']});
+						return jump(err);
+					}
+
+					res.send({status: 'success', template: 'UsergroupEdit', data: {usergroup: usergroup}});
+				});
+			});
+	});
 };
